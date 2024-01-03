e58a04e19ba548073b3a279abae493832485b39f
jk89 <jonathan.s.kelsey@gmail.com>
2024-01-03 13:12:38 +0000

    working on spherical distribution

---
+++ src/example8/src/index.ts
@@ -0,0 +1,272 @@
+import shaders from "./shaders/shaders.wgsl";
+import { mat4 } from "wgpu-matrix";
+
+// forked from https://compute.toys/view/282
+
+let frame_idx = 0;
+let frame_count = 0;
+let last_timestamp = 0;
+
+/*
+
+So what do we want for this pipeline?
+
+we want a binding to define 3d points
+we need a 4d camera matrix
+
+ */
+
+const main = async () => {
+
+    const fov = 60 * Math.PI / 180;
+    const near = 0.1;
+    const far = 1000;
+    const orbit_radius = 1;
+
+    const canvas = document.querySelector("#webgpu") as HTMLCanvasElement;
+    if (!navigator.gpu) return;
+    const context = canvas.getContext("webgpu") as GPUCanvasContext;
+    const adaptor = await navigator.gpu?.requestAdapter() as GPUAdapter;
+    if (!adaptor) return;
+    const format = adaptor.features.has('bgra8unorm-storage')
+      ? navigator.gpu.getPreferredCanvasFormat()
+      : 'rgba8unorm';
+
+    const device = await adaptor.requestDevice({
+        requiredFeatures: format === 'bgra8unorm'
+        ? ['bgra8unorm-storage']
+        : [],
+    }) as GPUDevice;   
+
+    context.configure({device, format,  usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.STORAGE_BINDING});
+
+    const compute_shader_module = device.createShaderModule(
+        {code: shaders}
+    );
+
+    const bind_group_layout = device.createBindGroupLayout({
+        label: "compute_group_bind_layout",
+        entries: [
+            {
+                binding: 0, // Texture binding
+                visibility: GPUShaderStage.COMPUTE,
+                storageTexture: {
+                    viewDimension: '2d', // View dimension based on your texture
+                    format: "bgra8unorm",
+                    access: "write-only"
+                },
+                
+                // storageTextureFormat: 'rgba8unorm', // Texture format
+            },
+            {
+                binding: 1, // Camera binding
+                visibility: GPUShaderStage.COMPUTE,
+                buffer: { 
+                    type: 'storage'
+                }
+            },
+            {
+                binding: 2, // Verts binding
+                visibility: GPUShaderStage.COMPUTE,
+                buffer: { 
+                    type: 'storage'
+                }
+            },
+            {
+                binding: 3, // Hist binding
+                visibility: GPUShaderStage.COMPUTE,
+                buffer: { 
+                    type: 'storage'
+                }
+            },
+            {
+                binding: 4, // Frame index binding
+                visibility: GPUShaderStage.COMPUTE,
+                buffer: {
+                    type: 'storage'
+                }
+            },
+            
+        ]  
+    });
+    
+
+    const pipeline_layout = device.createPipelineLayout({
+        bindGroupLayouts: [bind_group_layout]
+    });
+
+    const compute_splat_pipeline = device.createComputePipeline({
+        label: 'splat pipeline',
+        layout: pipeline_layout,
+        compute: {
+          module: compute_shader_module,
+          entryPoint: 'splat',
+        },
+      });
+
+    const compute_screen_pipeline = device.createComputePipeline({
+        label: 'write screen pipeline',
+        layout: pipeline_layout,
+        compute: {
+          module: compute_shader_module,
+          entryPoint: 'write_screen',
+        },
+      });
+
+      const tetrahedron_vertices = [
+        // Base triangle
+        0.0, 1.0, 0.0,
+        0.942809, -0.333333, 0.0,
+        -0.471405, -0.333333, -0.816497,
+      
+        // Side 1
+        0.0, 1.0, 0.0,
+        -0.471405, -0.333333, -0.816497,
+        0.0, -0.333333, 0.816497,
+      
+        // Side 2
+        0.0, 1.0, 0.0,
+        0.0, -0.333333, 0.816497,
+        0.942809, -0.333333, 0.0,
+      
+        // Side 3
+        0.942809, -0.333333, 0.0,
+        -0.471405, -0.333333, -0.816497,
+        0.0, -0.333333, 0.816497,
+      ];
+
+    function paint() {
+        const timestamp = performance.now()
+        const delta =  timestamp - last_timestamp;
+        
+
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const aspect = width / height;
+        const angle = frame_idx / 100;
+
+        const eye = [
+            Math.cos(angle) * orbit_radius, // x
+            5, // y Fixed height 
+            Math.sin(angle) * orbit_radius, // z
+        ];
+
+        const target = [0, 0, 0];
+        const up = [0, 1, 0];
+        const view = mat4.lookAt(eye, target, up);
+
+        const perspective = mat4.perspective(fov, aspect, near, far);
+        const view_projection_matrix = mat4.multiply(perspective, view);
+
+        // Create buffers for projection matrix and vectors list
+        const view_projection_buffer = device.createBuffer({
+            size: view_projection_matrix.length * Float32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+            mappedAtCreation: true,
+        });
+
+        new Float32Array(view_projection_buffer.getMappedRange()).set(view_projection_matrix);
+        view_projection_buffer.unmap();
+        
+        const vectors_buffer = device.createBuffer({
+            size: tetrahedron_vertices.length * Float32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+            mappedAtCreation: true,
+        });
+
+        new Float32Array(vectors_buffer.getMappedRange()).set(tetrahedron_vertices);
+        vectors_buffer.unmap();
+
+        const hist_size = width * height; // fix this
+        const hist_buffer = device.createBuffer({
+            size: hist_size * 4,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+        });
+
+        const frame_idx_buffer = device.createBuffer({
+            size: Uint32Array.BYTES_PER_ELEMENT,
+            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+        });
+        
+        const canvas_texture = context.getCurrentTexture();
+
+        const bind_group = device.createBindGroup(
+            {
+                layout:  bind_group_layout, // compute_screen_pipeline.getBindGroupLayout(0),//bind_group_layout,
+                entries: [
+                    { binding: 0, resource: canvas_texture.createView() },
+                    {binding: 1, resource: {
+                        buffer: view_projection_buffer,
+                        offset: 0,
+                        size: Float32Array.BYTES_PER_ELEMENT * 16
+                    }},
+                    {binding: 2, resource: {
+                        buffer: vectors_buffer,
+                        offset: 0,
+                        size: Float32Array.BYTES_PER_ELEMENT * tetrahedron_vertices.length
+                    }},
+                    {binding: 3, resource: {
+                        buffer: hist_buffer,
+                        offset: 0,
+                        size: Int32Array.BYTES_PER_ELEMENT * hist_size
+                    }},
+                    { 
+                        binding: 4, 
+                        resource: {
+                            buffer: frame_idx_buffer,
+                            offset: 0,
+                            size: Uint32Array.BYTES_PER_ELEMENT
+                        }
+                    },
+                ],
+            }
+        );
+
+        // Manually map and fill hist_buffer after creation
+        device.queue.writeBuffer(
+            hist_buffer,
+            0,
+            new Int32Array(hist_size).fill(0).buffer,
+            0,
+            hist_size * 4
+        );
+
+        device.queue.writeBuffer(
+            frame_idx_buffer,
+            0,
+            new Float32Array([frame_idx]).buffer,
+        );
+
+        const splat_command_encoder = device.createCommandEncoder();
+        const splat_compute_pass = splat_command_encoder.beginComputePass();
+        splat_compute_pass.setPipeline(compute_splat_pipeline);
+        splat_compute_pass.setBindGroup(0, bind_group);
+        splat_compute_pass.dispatchWorkgroups(64, 64, 12); // 64, 64, 12
+        splat_compute_pass.end();
+        const command_splat_buffer = splat_command_encoder.finish();
+        
+
+        const screen_command_encoder = device.createCommandEncoder();
+        const screen_compute_pass = screen_command_encoder.beginComputePass();
+        screen_compute_pass.setPipeline(compute_screen_pipeline);
+        screen_compute_pass.setBindGroup(0, bind_group);
+        screen_compute_pass.dispatchWorkgroups(canvas_texture.width, canvas_texture.height);
+        screen_compute_pass.end();    
+        const command_screen_buffer = screen_command_encoder.finish();
+        device.queue.submit([command_splat_buffer, command_screen_buffer]);
+        //frame_idx++;
+        frame_count++;
+
+        if (delta > 1000) {
+            console.log(`FPS: ${(frame_count)}`);
+            frame_count = 0;
+            last_timestamp = timestamp;
+        }
+        requestAnimationFrame(paint);        
+    };
+    setInterval(()=>frame_idx++,25);
+    requestAnimationFrame(paint);
+    
+};
+
+main();
---
+++ src/example8/src/shaders/shaders.wgsl
@@ -0,0 +1,90 @@
+@group(0) @binding(0) var screen: texture_storage_2d<bgra8unorm, write>;
+
+@group(0) @binding(1) var<storage, read_write> camera_matrix: mat4x4<f32>;
+
+@group(0) @binding(3) var<storage, read_write> hist_atomic: array<atomic<i32>>;
+
+@group(0) @binding(4) var<storage, read_write> frame_idx: f32;
+
+
+
+@compute @workgroup_size(16, 16) fn write_screen(@builtin(global_invocation_id) id: vec3u) {
+    let screen_dimensions = (textureDimensions(screen));
+    let hist_id = id.x + screen_dimensions.x * id.y;
+    let histogram_value = atomicLoad(&hist_atomic[hist_id]);
+    let modified_hist = log(f32(histogram_value/10));
+    let color = vec4f(f32(modified_hist), f32(modified_hist), f32(modified_hist), f32(modified_hist));
+    //vec4f(fract(vec2f(id.xy) / 320.0), f32(screen_dimensions.y), 1);
+    textureStore(screen, id.xy, color);
+}
+
+
+
+var<private> seed: u32;
+
+fn hash_u(_a: u32) -> u32{ var a = _a; a ^= a >> 16;a *= 0x7feb352du;a ^= a >> 15;a *= 0x846ca68bu;a ^= a >> 16;return a; }
+fn hash_f() -> f32{ var s = hash_u(seed); seed = s;return ( f32( s ) / f32( 0xffffffffu ) ); }
+fn hash_v2() -> vec2<f32>{ 
+    var h1: f32 = f32(hash_f());
+    var h2: f32 = f32(hash_f());
+    return vec2<f32>(h1, h2); 
+}
+fn hash_v3() -> vec3<f32>{ 
+    var h1: f32 = f32(hash_f());
+    var h2: f32 = f32(hash_f());
+    var h3: f32 = f32(hash_f());
+    return vec3<f32>(h1, h2, h3); 
+}
+fn hash_v4() -> vec4<f32>{ 
+    var h1: f32 = f32(hash_f());
+    var h2: f32 = f32(hash_f());
+    var h3: f32 = f32(hash_f());
+    var h4: f32 = f32(hash_f());
+    return vec4<f32>(h1, h2, h3, h4); 
+}
+
+@compute @workgroup_size(256, 1,1)
+fn splat(@builtin(global_invocation_id) id: vec3<u32>) {
+    let screen_dimensions = vec2<u32>(textureDimensions(screen));
+    if (id.x >= screen_dimensions.x || id.y >= screen_dimensions.y) { return; }
+
+    var proj_matrix_element = camera_matrix[0][0] + camera_matrix[0][1] + camera_matrix[0][2] + camera_matrix[0][3] +
+    camera_matrix[1][0] + camera_matrix[1][1] + camera_matrix[1][2] + camera_matrix[1][3] +
+    camera_matrix[2][0] + camera_matrix[2][1] + camera_matrix[2][2] + camera_matrix[2][3] +
+    camera_matrix[1][0] + camera_matrix[1][1] + camera_matrix[2][2] + camera_matrix[3][3];
+
+    // get a random point.
+    seed = hash_u(id.x + hash_u(screen_dimensions.x*id.y*200u)*20u + hash_u(id.x)*250u + hash_u(id.z)*250u + hash_u(u32(frame_idx))*250u  );
+    seed = hash_u(seed);
+    var p = hash_v3()*2. - 1.;
+    let sigma = 1.0;
+
+    // Normalize the vector to ensure it lies on the unit sphere
+    let length = 1/sqrt(dot(p,p));
+    var p2 = vec3<f32>(p.x / length, p.y / length, p.z / length);
+
+    let u = hash_f();
+    let radius = 1.0; //sigma * sqrt(-2.0 * log(1.0 - u));
+
+    // Scale the direction by the radius
+    p = p2 * radius;
+
+        //p = vec3<f32>(scaledX, scaledY, scaledZ);
+
+   // p = vec3<f32>(x, y, z);
+    
+    var transformed_point = camera_matrix * vec4<f32>(p,1);
+    var normalized_point: vec3<f32> = transformed_point.xyz / transformed_point.w;
+
+    // Calculate the pixel index from the point 'p' in the range [-1, -1] to [1, 1]
+    var pixelX = i32((normalized_point.x + 1.0) * 0.5 * f32(screen_dimensions.x));
+    var pixelY = i32((normalized_point.y + 1.0) * 0.5 * f32(screen_dimensions.y));
+
+    if (pixelX < 0 || pixelX >= i32(screen_dimensions.x) || pixelY < 0 || pixelY >= i32(screen_dimensions.y) || normalized_point.z < 0) {
+        return; // Exit the computation early if outside the screen dimensions
+    }
+
+    // Combine 'pixelX' and 'pixelY' into a single index
+    var idx = pixelY * i32(screen_dimensions.x) + pixelX;
+    atomicAdd(&hist_atomic[idx],1);
+}
